8a9c6cdd091299aca45f98e8f1044976121d253c
Tom Pohorily <sebastianbelle074@gmail.com>
2026-05-01 19:42:46 -0500

    refactor: move scrollbar/main panel to ValkyrieLib

--- UI/QuickModConfigPanel.cs
+++ UI/QuickModConfigPanel.cs
@@ -5,22 +5,22 @@ using Terraria;
 using Terraria.GameContent.UI.Elements;
 using Terraria.ID;
 using Terraria.ModLoader.UI;
+using Terraria.ModLoader.UI.Elements;
 using Terraria.UI;
 using ValkyrieLib;
 
 namespace QuickModConfig;
 
-public class QuickModConfigPanel : UIState, IBlocksInput, IHasCloseButton
+public class QuickModConfigPanel : UIState, IBlocksInput, IHasCloseButton, IHasScrollbar, IHasMainPanel
 {
-    public UIElement MainElement { get; private set; } = null!;
+    public UIElement MainElement { get; set; } = null!;
+    public UIElement ScrollViewElement { get; private set; } = null!;
 
     public event Action CloseRequested = null!;
 
     private readonly List<ModConfigDataGroup> _modData;
 
-    private UIPanel _panel = null!;
-    private UIScrollbar _uiScrollbar = null!;
-    private bool _scrollBarVisible;
+    private UIList _uiList = null!;
 
     public QuickModConfigPanel()
     {
@@ -29,36 +29,20 @@ public class QuickModConfigPanel : UIState, IBlocksInput, IHasCloseButton
 
     public override void OnInitialize()
     {
-        _panel = new UIPanel();
-        _panel.Width.Set(600, 0f);
-        _panel.Height.Set(300, 0f);
-        _panel.BackgroundColor = ValkyrieAPI.UI.Colors.LightBackground;
-        _panel.BorderColor = ValkyrieAPI.UI.Colors.Border;
-        _panel.HAlign = 1f;
-        _panel.VAlign = 1f;
-        _panel.Left = StyleDimension.FromPixels(-10);
-        _panel.Top = StyleDimension.FromPixels(-10);
-        Append(_panel);
+        MainElement.Width.Set(600, 0f);
+        MainElement.Height.Set(300, 0f);
+        MainElement.HAlign = 1f;
+        MainElement.VAlign = 1f;
+        MainElement.Left = StyleDimension.FromPixels(-10);
+        MainElement.Top = StyleDimension.FromPixels(-10);
 
-        var uiList = new UIList()
+        Append(MainElement);
+
+        _uiList = new UIList()
         {
             Width = StyleDimension.Fill,
             Height = StyleDimension.Fill
         };
-
-        const float ScrollBarTopInset = 27;
-        const float ScrollBarBottomInset = 3;
-        const float ScrollBarLeftInset = 5;
-
-        _uiScrollbar = new UIScrollbar()
-        {
-            Height = StyleDimension.FromPixelsAndPercent(-ScrollBarTopInset - ScrollBarBottomInset, 1f),
-            Top = StyleDimension.FromPixels(ScrollBarTopInset),
-            Left = StyleDimension.FromPixels(ScrollBarLeftInset),
-            HAlign = 1f
-        };
-        _scrollBarVisible = true;
-        uiList.SetScrollbar(_uiScrollbar);
 
         foreach (var mod in _modData)
         {
@@ -74,29 +58,16 @@ public class QuickModConfigPanel : UIState, IBlocksInput, IHasCloseButton
                 PaddingRight = HorizontalPadding,
             };
             
-            uiList.Add(btn);
+            _uiList.Add(btn);
         }
 
-        _panel.Append(uiList);
-        _panel.Append(_uiScrollbar);
+        MainElement.Append(_uiList);
 
-        MainElement = _panel;
+        ScrollViewElement = _uiList;
     }
 
-    public override void Update(GameTime gameTime)
+    public void SetScrollbar(UIScrollbar scrollbar)
     {
-        base.Update(gameTime);
-
-        bool canScroll = _uiScrollbar.CanScroll;
-
-        if (canScroll != _scrollBarVisible)
-        {
-            if (canScroll)
-                _panel.Append(_uiScrollbar);
-            else
-                _uiScrollbar.Remove();
-
-            _scrollBarVisible = canScroll;
-        }
+        _uiList.SetScrollbar(scrollbar);
     }
 }
