c9e4b642404ce13c3d252ec6b8a9db64b69ca3e6
Tom Pohorily <sebastianbelle074@gmail.com>
2026-05-04 12:12:35 -0500

    refactor: stop using Krafs.Publicizer method

--- DragonLensIntegration.cs
+++ DragonLensIntegration.cs
@@ -4,6 +4,8 @@ using Terraria.ModLoader;
 using DragonLens.Core.Systems.ThemeSystem;
 using DragonLens.Core.Systems.ToolbarSystem;
 using DragonLens.Core.Systems.ToolSystem;
+using Microsoft.Xna.Framework;
+using System;
 
 namespace QuickModConfig;
 
@@ -15,27 +17,44 @@ public class QuickModConfigTool : Tool
     public override string DisplayName => "Quick config";
     public override string Description => "Quickly edit configs.";
 
+    // Gear icon by MSavioti from https://opengameart.org/content/gear-pixel-art-32x32 licensed CC0
+    private static readonly Texture2D _gearIcon = ModContent.Request<Texture2D>("QuickModConfig/Assets/Textures/Gear", AssetRequestMode.ImmediateLoad).Value;
+
     public override void OnActivate()
     {
         QuickModConfig.ModHandle.ToggleUI("Quick Mod Config");
     }
-}
 
-[JITWhenModsEnabled("DragonLens")]
-[ExtendsFromMod("DragonLens")]
-public class DragonLensIconIntegration : ModSystem
-{
-    public override void PostSetupContent()
+    public override void DrawIcon(SpriteBatch spriteBatch, Rectangle target)
     {
-        if (!ModLoader.TryGetMod("DragonLens", out _))
-            return;
+        var scale = Math.Min((float)target.Width / _gearIcon.Width, (float)target.Height / _gearIcon.Height);
 
-        // Gear icon by MSavioti from https://opengameart.org/content/gear-pixel-art-32x32 licensed CC0
-        var tex = ModContent.Request<Texture2D>($"{Mod.Name}/Assets/Textures/Gear", AssetRequestMode.ImmediateLoad).Value;
+        var position = new Vector2(
+            target.X + ((target.Width - (_gearIcon.Width * scale)) / 2f),
+            target.Y + ((target.Height - (_gearIcon.Height * scale)) / 2f)
+        );
 
-        foreach (var provider in ThemeHandler.allIconProviders.Values)
-            provider.icons["QuickModConfig"] = tex;
-
-        ModContent.GetInstance<ToolbarHandler>().OnModLoad();
+        spriteBatch.Draw(_gearIcon, position, null, Color.White, 0f, Vector2.Zero, scale, SpriteEffects.None, 0f);
     }
 }
+
+// IMPORTANT: Commented out because Krafs.Publicizer stopped working for some reason!
+
+// [JITWhenModsEnabled("DragonLens")]
+// [ExtendsFromMod("DragonLens")]
+// public class DragonLensIconIntegration : ModSystem
+// {
+//     public override void PostSetupContent()
+//     {
+//         if (!ModLoader.TryGetMod("DragonLens", out _))
+//             return;
+
+//         // Gear icon by MSavioti from https://opengameart.org/content/gear-pixel-art-32x32 licensed CC0
+//         var tex = ModContent.Request<Texture2D>($"{Mod.Name}/Assets/Textures/Gear", AssetRequestMode.ImmediateLoad).Value;
+
+//         foreach (var provider in ThemeHandler.allIconProviders.Values)
+//             provider.icons["QuickModConfig"] = tex;
+
+//         ModContent.GetInstance<ToolbarHandler>().OnModLoad();
+//     }
+// }
